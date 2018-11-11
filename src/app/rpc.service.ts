4363265bbf44dae3c641314e80d8ee51dac42809
Kewde <kewde@particl.io>
2018-11-11 02:33:58 +0100

    Use environments (dev points to my server, production builds to local url)

--- src/app/rpc.service.ts
+++ src/app/rpc.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class RpcService {
-  private customUrl =  '';// 'http://66.172.10.231:901'; // http://144.202.17.55
-  private testnet = false;
 
   constructor(private http: HttpClient) { }
 
   get url(): string {
-    return `${this.customUrl}${this.testnet ? '/api/test' : '/api'}`;
+    return `${environment.url}${environment.testnet ? '/api/test' : '/api'}`;
   }
   // Uses http.get() to load data from a single API endpoint
   getConfig() {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'http://144.202.17.55',
+  testnet: false
 };
